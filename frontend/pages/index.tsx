516689e74a181596b41869f9567fa8324bbb3ea4
abdul hamid <abdulachik@icloud.com>
2023-01-20 23:03:16 -0600

    renaming project to django-nextjs-boilerplate and fixing test not passing prior to cookiecutter

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,14 @@
 import Head from 'next/head';
-import {useSession} from "next-auth/react";
-
 export default function Home() {
-    const {data: session} = useSession();
-
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
             <Head>
                 <title>Django and Next.js Project Template</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
 
             <div className="mx-auto sm:max-w-xl flex items-center justify-center flex-col">
                 <h1 className="text-3xl font-medium text-gray-900">Django and Next.js Project Template</h1>
-                {session?.user && (
-                    <p className="text-gray-700">Welcome {session?.user?.email}</p>
-                )}
                 <p className="text-gray-700 my-4">
                     The project is structured with two separate directories for the frontend and backend.
                     The backend, which is built using Django REST framework, is located in the `./backend` directory,
@@ -24,11 +17,13 @@ export default function Home() {
                     where you can find the pages, styles, and server files.
                 </p>
                 <p className="text-gray-700 my-4">
-                    This template is designed to make it easy to quickly bootstrap a full stack application following modern practices.
+                    This template is designed to make it easy to quickly bootstrap a full stack application following
+                    modern practices.
                     It includes features such as authentication with next-auth.js and Django REST framework simplejwt,
                     Docker-compose for easy setup and deployment, automatic API documentation generation, and more.
                 </p>
-                <a href="https://github.com/sicksid/django-nextjs-project-template" className="inline-block py-2 px-4 rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                <a href="https://github.com/sicksid/django-nextjs-project-template"
+                   className="inline-block py-2 px-4 rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                     View on GitHub
                 </a>
             </div>
